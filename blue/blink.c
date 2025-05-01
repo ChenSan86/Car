10e66b8517b256b3753c869d2a77852dc8699926
ChenSan <chensan0039@gmail>
2025-05-01 14:13:29 +0800

    finish bluetooth move test,going to complete net problem

---
+++ blue/blink.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <wiringx.h>
+#include <stdio.h>
+#include "blink.h"
+int counter = 2;
+int LED = 25;
+void initBlink(){
+    if (wiringXValidGPIO(LED) != 0)
+    {
+        printf("Invalid GPIO %d\n", LED);
+    }
+
+    pinMode(LED, PINMODE_OUTPUT);
+}
+void blink(){
+    while (counter--)
+    {
+        printf("Duo LED GPIO (wiringX) %d: High\n", LED);
+        digitalWrite(LED, HIGH);
+        sleep(1);
+        printf("Duo LED GPIO (wiringX) %d: Low\n", LED);
+        digitalWrite(LED, LOW);
+        sleep(1);
+    }
+}
---
+++ blue/move.h
@@ -0,0 +1,26 @@
+#ifndef MOVE_H
+#define MOVE_H
+#include <stdio.h>
+#include <wiringx.h>
+#include <unistd.h>
+#define EN1 14 // forward = 1
+#define EN2 15
+#define PWM_1 4
+#define PWM_2 5
+#define EN3 16 // forward = 1
+#define EN4 17
+#define PERIOD 500000 // 500us
+#define T 5000
+#include "wiringx.h"
+extern int speedr, speedl;
+
+void initMove();
+void Move(float l, float r);
+void brake();
+void forward();
+void back();
+void left();
+void right();
+void rotationR();
+void rotationL();
+#endif
